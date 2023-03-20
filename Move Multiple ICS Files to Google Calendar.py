51e36b49940cd427a9fc64e154d5c58fbe4bd3c7
Justin Agramonte <JustinAgramonte@gmail.com>
2023-03-20 01:00:04 -0400

    changing filename input to selector, work with R on webpage API

--- Move Multiple ICS Files to Google Calendar.py
+++ Move Multiple ICS Files to Google Calendar.py
@@ -1,16 +1,24 @@
 #Given a folder of .ICS files, import them to Google Calendar
 #HTTP Requests:
-'''Google HTTP Requests: https://developers.google.com/calendar/api/v3/reference/events/import#http-request, 
-https://developers.google.com/calendar/api/v3/reference/events/import#request-body, https://developers.google.com/calendar/api/v3/reference/events#resource  
-'''
-#Google Auth: https://developers.google.com/workspace/guides/auth-overview, https://developers.google.com/identity/gsi/web/guides/overview
-#Google Calendar Week to Target: https://calendar.google.com/calendar/u/0/r/week/2023/2/3
+'''Google HTTP Requests: https://developers.google.com/calendar/api/v3/reference/events/import#request-body, 
+https://developers.google.com/calendar/api/v3/reference/events#resource'''
+
+'''Google Auth: https://developers.google.com/workspace/guides/auth-overview,
+Google Calendar Week to Target: https://calendar.google.com/calendar/u/0/r/week/2023/2/3'''
 
 import os, requests
 
-filePath = input("Enter filepath: ").strip('\"')
-assert os.path.isdir(filePath), "Filepath not found...copy from explorer path?"
+while True:
+    filePath = input("Enter filepath: ").strip('\"')
+    if os.path.isdir(filePath):
+        break
+    else:
+        print("Filepath not found...copy from explorer path?")
+
 
 for file in os.listdir(filePath): #opening file
-    #placeholder
-    print
+    OpenJSON = open(os.path.join(filePath, file), 'r', encoding="utf8")
+    ICS_JSON = OpenJSON.read()
+    GetReq = requests.get('https://www.googleapis.com/calendar/v3/users/me/calendarList')
+    #GetReq = requests.get('https://www.googleapis.com/calendar/v3/calendars/primary/events/import', data = ICS_JSON, auth = "")
+    print(GetReq.status_code, GetReq.text)
